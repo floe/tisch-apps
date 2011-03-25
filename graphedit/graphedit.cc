24210c1e3321606d898115f866f1aa0c76a38e26
Florian Echtler <floe@butterbrot.org>
2011-03-25 12:26:47 +0000

    - some makefile fixes - updated graph editor
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/applets@265 28952012-f250-0410-9090-e835bad21ca5

--- graphedit/graphedit.cc
+++ graphedit/graphedit.cc
@@ -14,13 +14,14 @@
 #include <BlobCount.h>
 
 class CloseButton;
+class Node;
 
 struct Edge {
-	Tile* n1;
-	Tile* n2;
+	Node* n1;
+	Node* n2;
 };
 
-std::vector<Tile*> node_list;
+std::vector<Node*> node_list;
 std::map<CloseButton*,Edge> edge_list;
 
 
@@ -37,6 +38,104 @@ class CloseButton: public Button {
 		}
 };
 
+
+class Node: public Tile {
+	public:
+
+		Node( int _x, int _y, RGBATexture* _tex ):
+		Tile( 100, 100, _x, _y, 0, _tex, TISCH_TILE_MOVE|TISCH_TILE_SLIDE|TISCH_TILE_BOUNCE) {
+			shadow = false;
+
+			for(std::vector<Gesture>::iterator it = region.gestures.begin(); it != region.gestures.end(); it++) {
+				if(it->name() == "move") { region.gestures.erase(it); break; }
+			}
+
+			Gesture move( "move", GESTURE_FLAGS_STICKY );
+
+			move.push_back(new Motion());
+			std::vector<int> b1; b1.push_back( 1 ); b1.push_back( 1 );
+			BlobCount* bcnt1 = new BlobCount(1<<INPUT_TYPE_FINGER);
+			bcnt1->bounds( b1 );
+			move.push_back( bcnt1 );
+			region.gestures.push_back( move );	
+
+			Gesture link( "link", GESTURE_FLAGS_STICKY );
+
+			link.push_back(new Motion());
+			std::vector<int> b2; b2.push_back( 2 ); b2.push_back( 2 );
+			BlobCount* bcnt2 = new BlobCount(1<<INPUT_TYPE_FINGER);
+			bcnt2->bounds( b2 );
+			link.push_back( bcnt2 );
+			region.gestures.push_back( link );	
+
+			lx = x; ly = y; do_link = 0;
+		}
+
+		void tap( Vector pos, int id ) {
+			lx = x;
+			ly = y;
+		}
+
+		void release( ) {
+			if (do_link) {
+				std::cout << "testing: " << lx << " " << ly << std::endl;
+				for (std::vector<Node*>::iterator node = node_list.begin(); node != node_list.end(); node++) {
+					std::cout << "node: " << (*node)->x << " " << (*node)->y << std::endl;
+					if ((*node)->inside( lx, ly )) {
+						std::cout << "inside " << std::endl;
+						CloseButton* tmp = new CloseButton( 20, 20, 100, 100 );
+						tmp->region.flags( REGION_FLAGS_VOLATILE | (1 << INPUT_TYPE_FINGER) );
+						parent->add( tmp );
+						edge_list[tmp] = { this, *node };
+						break;
+					}
+				}
+			}
+			lx = x;
+			ly = y;
+			do_link = 0;
+		}
+
+		void draw() {
+
+			if (do_link) {
+				glLineWidth(2.0);
+				glColor4f(0.0,1.0,0.0,1.0);
+
+				glBegin(GL_LINES);
+					glVertex2f(x,y);
+					glVertex2f(lx,ly);
+				glEnd();
+			}
+
+			enter();
+
+			Widget::paint();
+
+			leave();
+		}
+
+		bool inside( int _x, int _y ) {
+			double dx = x - _x;
+			double dy = y - _y;
+			return (sqrt(dx*dx+dy*dy)<50);
+		}
+
+
+		void action( Gesture* gst ) {
+			if (gst->name() == "link") {
+				FeatureBase* f = (*gst)[0];
+				Motion* m = dynamic_cast<Motion*>(f);
+				Vector tmp = m->result();
+				transform(tmp,0,1);
+				lx += tmp.x;
+				ly += tmp.y;
+				do_link = 1;
+			} else Tile::action( gst );
+		}
+
+		int lx, ly, do_link;
+};
 
 
 class GraphWin: public Window {
@@ -80,8 +179,8 @@ public:
 
 int main( int argc, char* argv[] ) {
 
-	std::cout << "graph - libTISCH 1.1 widget layer demo" << std::endl;
-	std::cout << "(c) 2010 by Florian Echtler <echtler@in.tum.de>" << std::endl;
+	std::cout << "graphedit - libTISCH 2.0 widget layer demo" << std::endl;
+	std::cout << "(c) 2011 by Florian Echtler <floe@butterbrot.org>" << std::endl;
 
 	int mouse = ((argc > 1) && (std::string("-m") == argv[1]));
 	GraphWin* win = new GraphWin( 800, 480, "Graph", mouse );
@@ -92,12 +191,11 @@ int main( int argc, char* argv[] ) {
 	RGBATexture* tmp = new RGBATexture( TISCH_PREFIX "Dial.png" );
 
 	for (int i = 0; i < 20; i++) {
-		Tile* foo = new Tile( 50, 50,
+		Node* foo = new Node(
 			(int)(((double)random()/(double)RAND_MAX)*700-350),
 			(int)(((double)random()/(double)RAND_MAX)*450-225),
-			0, tmp, TISCH_TILE_MOVE|TISCH_TILE_SLIDE|TISCH_TILE_BOUNCE
+			tmp
 		);
-		foo->shadow = false;
 		win->add( foo );
 		node_list.push_back(foo);
 	}
