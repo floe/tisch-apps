c2248c5d7c4df1f8c04435d9ad5560a1b9cd1495
Norbert Wiedermann <wiederma@in.tum.de>
2012-06-12 14:19:06 +0000

    TCPServer offers a main thread to listen for incoming TCP connections a incoming connection is handled as a TCPRequest in a new thread so server can continue listening TODO: exchange byte messages, create list of markerID and attach pointer to TCP RequestThread to markerID when identified
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/applets@437 28952012-f250-0410-9090-e835bad21ca5

--- Datatransfer/datatransfer.cpp
+++ Datatransfer/datatransfer.cpp
@@ -3,6 +3,22 @@
 *     Copyright (c) 2012 by Norbert Wiedermann, <wiederma@in.tum.de>      *
 *   Licensed under GNU Lesser General Public License (LGPL) 3 or later    *
 \*************************************************************************/
+#pragma once
+#ifdef _MSC_VER
+	#define _AFXDLL
+	#include <Afxwin.h>
+	#include <WinSock2.h>
+	#include <WinSock.h>
+	typedef int socklen_t;
+	
+#else
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#define	INVALID_SOCKET -1
+	#define	SOCKET_ERROR -1
+	#define SOCKET int
+	#define SOCKADDR struct sockaddr
+#endif
 
 #include <stdlib.h>
 #include <nanolibc.h>
@@ -15,14 +31,9 @@
 #include "BlobCount.h"
 #include "BlobPos.h"
 
-#include "Socket.h"
-
-
 using namespace std;
 
 Window* win = 0;
-
-
 
 class HandyDropZone: public Tile {
 public: HandyDropZone( int _x, int _y, RGBATexture* _tex):
@@ -98,7 +109,120 @@ public:
 	} // void action( Gesture* gesture ) {
 };
 
+class TcpRequestThread {
+public:
+	static UINT TcpRequestThreadStaticEntryPoint(LPVOID pThis) {
+		TcpRequestThread* pthisTcpRequest = (TcpRequestThread*) pThis;
+		pthisTcpRequest->TcpRequestThreadEntryPoint();
+		return 1;
+	}
 
+	void TcpRequestThreadEntryPoint() {
+		cout << "handle request" << endl;
+		// =========================================================
+		// from client
+		// =========================================================
+
+		// =========================================================
+		// to client
+		// =========================================================
+		cout << "done" << endl;
+	}
+};
+
+class TcpServerThread {
+public:
+	SOCKET AcceptSocket;
+	SOCKET ListenSocket;
+	int PORT;
+	sockaddr_in local;
+	sockaddr_in from;
+
+	static UINT TcpServerThreadStaticEntryPoint(LPVOID pThis) {
+		TcpServerThread* pthisTcpServer = (TcpServerThread*)pThis;
+		pthisTcpServer->PORT = 8080;
+		pthisTcpServer->TcpServerThreadEntryPoint();
+		return 0;
+	}
+
+	void TcpServerThread::initNetwork() {
+		cout << "starting up TCP server ... ";
+
+		WSADATA wsaData;
+	
+		int wsaret = WSAStartup(MAKEWORD(2,2), &wsaData);
+		if(wsaret != 0) {
+			cout << endl << "WSAStartup failed" << endl;
+			exit(1);
+		}
+
+		local.sin_family = AF_INET;
+		local.sin_addr.s_addr = INADDR_ANY;
+		local.sin_port = htons(PORT);
+
+		ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+		if(ListenSocket == INVALID_SOCKET) {
+			cout << endl << "socket() failed" << endl;
+	#ifdef _MSC_VER
+			WSACleanup();
+	#endif
+			exit(2);
+		}
+
+		if(bind(ListenSocket, (sockaddr*)&local, sizeof(local)) != 0) {
+			cout << endl << "bind() failed" << endl;
+#ifdef _MSC_VER
+		WSACleanup();
+#endif
+			exit(3);
+		}
+
+		if(listen(ListenSocket, 10) != 0) {
+			cout << endl << "listen() failed" << endl;
+	#ifdef _MSC_VER
+			WSACleanup();
+	#endif
+			exit(4);
+		}
+	}
+
+	void TcpServerThread::TcpServerThreadEntryPoint() {
+
+		initNetwork();
+
+		int fromlen = sizeof(from);
+
+		cout << "waiting for connections" << endl;
+		while(true) {
+			char temp[512];
+			AcceptSocket = accept(ListenSocket, (struct sockaddr*) &from, &fromlen);
+			if (AcceptSocket == INVALID_SOCKET) {
+				cout << endl << "accept() failed" << endl;
+				closesocket(ListenSocket);
+	#ifdef _MSC_VER
+				WSACleanup();
+	#endif
+				exit(5);
+			}
+
+			TcpRequestThread* request = new TcpRequestThread();
+			AfxBeginThread(TcpRequestThread::TcpRequestThreadStaticEntryPoint, (void*)request);
+
+			sprintf(temp, "Your IP is %s\r\n",inet_ntoa(from.sin_addr));
+			send(AcceptSocket, temp, strlen(temp), 0);
+			cout << "Connection from " << inet_ntoa(from.sin_addr) << endl;
+			closesocket(AcceptSocket);
+		}
+
+		closesocket(ListenSocket);
+	#ifdef _MSC_VER
+		WSACleanup();
+	#endif
+		exit(0);
+	}
+
+};
 
 int main( int argc, char* argv[] ) {
 	cout << "Datatransfer - libTISCH demo" << endl;
@@ -113,9 +237,14 @@ int main( int argc, char* argv[] ) {
 
 	Handy* handy = new Handy( width, height );
 	win->add(handy);
-	
-	
 
 	win->update();
+	
+	// start TCP Server waiting for connections
+	TcpServerThread* server = new TcpServerThread();
+	AfxBeginThread(TcpServerThread::TcpServerThreadStaticEntryPoint, (void*)server);
+	
+	// keep looping on window thread
 	win->run();
+
 }
