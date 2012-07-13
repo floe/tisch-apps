d69e391baafb43215b50334826f79de7fb80cb3d
Florian Echtler <floe@butterbrot.org>
2012-07-13 21:24:43 +0000

    hide buttons
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/applets@458 28952012-f250-0410-9090-e835bad21ca5

--- siedler/siedler.cc
+++ siedler/siedler.cc
@@ -14,14 +14,30 @@
 #include "Dial.h"
 #include "Label.h"
 
+
+/*
+ * TODO:
+ *
+ * add labels with number in center of each hexagon (how to handle desert?)
+ * calculate percentages, add labels to each crossing
+ *
+ */
+
+int bsize = 15;
+RGBATexture* empty;
+
 // This class demonstrates how to react to the predefined tap gesture using Button as superclass.
 class MyButton: public Button {
 	public:
 
-		MyButton( int _w, int _h, int _x = 0, int _y = 0, double angle = 0.0, RGBATexture* _tex = 0 ):
-			Button( _w,_h,_x,_y,angle,_tex)
+		MyButton( int _x = 0, int _y = 0 ):
+			Button( bsize,bsize,_x,_y,0.0,empty)
 		{
-			lbl = new Label("xyz",50,20,x+50,y+50);
+			mycolor[0] = 1.0;
+			mycolor[1] = 1.0;
+			mycolor[2] = 1.0;
+			mycolor[3] = 0.0;
+			lbl = new Label(" ",50,20,x+25,y+25);
 		}
 
 		void init() { parent->add(lbl); }
@@ -32,25 +48,25 @@ class MyButton: public Button {
 		Label* lbl;
 };
 
-int bsize = 15;
-
 int main( int argc, char* argv[] ) {
 
 	std::cout << "siedler - based on libTISCH 2.0" << std::endl;
 	std::cout << "(c) 2012 by Florian Echtler <floe@butterbrot.org>" << std::endl;
 
+	empty = new RGBATexture("empty.png");
+
 	// create a new window: width, height, title, use_mouse
 	Window* win = new Window( 800, 600, "siedler", (argc > 1) );
 	win->texture( new RGBATexture("siedler.png") );
 
-	MyButton* btn1 = new MyButton( bsize,bsize,   0,-33.5 ); win->add( btn1 ); btn1->init();
-	MyButton* btn2 = new MyButton( bsize,bsize,   0, 33.5 ); win->add( btn2 ); btn2->init();
+	MyButton* btn1 = new MyButton(   0,-33.5 ); win->add( btn1 ); btn1->init();
+	MyButton* btn2 = new MyButton(   0, 33.5 ); win->add( btn2 ); btn2->init();
 
-	MyButton* btn3 = new MyButton( bsize,bsize,  29,-17.5 ); win->add( btn3 ); btn3->init();
-	MyButton* btn4 = new MyButton( bsize,bsize,  29, 17.5 ); win->add( btn4 ); btn4->init();
+	MyButton* btn3 = new MyButton(  29,-17.5 ); win->add( btn3 ); btn3->init();
+	MyButton* btn4 = new MyButton(  29, 17.5 ); win->add( btn4 ); btn4->init();
 
-	MyButton* btn5 = new MyButton( bsize,bsize, -29,-17.5 ); win->add( btn5 ); btn5->init();
-	MyButton* btn6 = new MyButton( bsize,bsize, -29, 17.5 ); win->add( btn6 ); btn6->init();
+	MyButton* btn5 = new MyButton( -29,-17.5 ); win->add( btn5 ); btn5->init();
+	MyButton* btn6 = new MyButton( -29, 17.5 ); win->add( btn6 ); btn6->init();
 
 	win->update();
 	win->run();
---
+++ siedler/siedler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DISPLAY=:0.0
+export LD_LIBRARY_PATH=$(pwd)/../../libtisch-2.0/build/lib/
+
+cd ../../libtisch-2.0/build/bin/
+./touchd -V -c ../../../applets/siedler/kinect.xml > /dev/null & disown
+sleep 10
+./calibd & disown
+
+cd ../../../applets/siedler
+./siedler
